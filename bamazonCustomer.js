58f0886ddee967040d7ee19630712f1e8d2758fa
Kilea Castillo <kileaw@gmail.com>
2020-05-11 10:07:26 -0600

    got the return working. thank you joe!

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,9 +1,9 @@
-//npm packages
+// npm packages
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 require("console.table");
 
-//syncing with mysql database
+// syncing with mysql database
 var connection = mysql.createConnection({
     host: "localhost",
 
@@ -15,7 +15,7 @@ var connection = mysql.createConnection({
     database: "bamazon_db"
 });
 
-//connection made with mysql server and loads stock 
+// connection made with mysql server and loads stock 
 connection.connect(function (err) {
     if (err) {
         console.error("error connecting: " + err.stack);
@@ -23,7 +23,7 @@ connection.connect(function (err) {
     displayStock();
 });
 
-//basically makes sure the user input is a number
+// basically makes sure the user input is a number
 function validateInput(value) {
 	var integer = Number.isInteger(parseFloat(value));
 	var sign = Math.sign(value);
@@ -35,8 +35,8 @@ function validateInput(value) {
 	}
 }
 
-//displays stock items from database
-//calls for the prompting customer event
+// displays stock items from database
+// calls for the prompting customer event
 function displayStock() {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
@@ -45,9 +45,9 @@ function displayStock() {
     });
 }
 
-//inquirer prompt to prompt user for what item they want to purchase 
-//validates for a number 
-//turns choice into a string and 
+// inquirer prompt to prompt user for what item they want to purchase 
+// validates for a number 
+// turns choice into a string and 
 function promptForUser(inventory) {
     inquirer
         .prompt([{
@@ -68,9 +68,9 @@ function promptForUser(inventory) {
         });
 }
 
-//simple for loop finding the stock item, comparing if it's there and returning the index of inventory
-//which is the customers item
-//otherwise we return null for missing inventory
+// simple for loop finding the stock item, comparing if it's there and returning the index of inventory
+// which is the customers item
+// otherwise we return null for missing inventory
 function checkStock(choiceID, inventory) {
     for (var i = 0; i < inventory.length; i++) {
         if (inventory[i].item_id === choiceID) {
@@ -80,10 +80,10 @@ function checkStock(choiceID, inventory) {
     return null;
 }
 
-//prompt for how many
-//input quantity and coverts to a string
-//if that string is more than what we have in stock we send a message
-//otherwise we go to buy the product at the quantity the customer wants
+// prompt for how many
+// input quantity and coverts to a string
+// if that string is more than what we have in stock we send a message
+// otherwise we go to buy the product at the quantity the customer wants
 function quantityPrompt(product) {
     inquirer.prompt([{
             type: "input",
@@ -102,9 +102,9 @@ function quantityPrompt(product) {
         });
 }
 
-//connects to database, updates database with item purchased and how much
+// connects to database, updates database with item purchased and how much
 // sends message based on quantity and name
-// attempt at replaying the whole thing
+// returns you to the main menu (displayStock)
 function buy(product, quantity) {
     connection.query(
         "UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?",
@@ -112,20 +112,18 @@ function buy(product, quantity) {
         function (err, res) {
             console.log("You have just purchased " + quantity + " " + product.product_name);
             console.log("Your total is: " + product.price * quantity);
-            // this does not work correctly ---------
             inquirer.prompt([{
                 type: "confirm",
                 name: "replay",
                 message: "Would you like to purchase more?",
                 default: false,
-            }]).then(function (replay) {
-                // console.log(replay)
-                if (replay === true) {
+            }]).then(function (response) {
+                console.log(response.replay);
+                if (response.replay === true) {
                     displayStock();
-                }else {
-                    console.log("See ya next time!");
+                } else {
+                    console.log('See ya next time!');
                 }
             });
-            //----------- it's supposed to return the customer to the main menu (displayStock)
         });
 }
